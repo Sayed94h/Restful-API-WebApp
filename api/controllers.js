cd1a0b942cb1fcc84d8d79a8e12a6ff71156a5d7
sayed94h <skazimi951@yahoo.com>
2020-11-05 22:09:46 +0100

    finished the main part of getting and displaying the data

--- api/controllers.js
+++ api/controllers.js
@@ -1,16 +1,46 @@
-'use strict'
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Joi = require('joi');
+const fs = require("fs");
+const path = require("path");
+const Joi = require("joi");
 
-const config = require('../config');
-const DATA_DIR = path.join(__dirname, '..', 'data', 'courses.json');
+const config = require("../config");
+const DATA_DIR = path.join(__dirname, "..", "data", "courses.json");
 
 const controllers = {
-  hello: (req, res) => {
-    res.json({ api: 'courses!' });
-  }
+	hello: (req, res) => {
+		res.json({ api: "courses!" });
+	},
+	getCourses: (req, res, next) => {
+		fs.readFile(DATA_DIR, "UTF-8", (err, data) => {
+			if (err) {
+				console.error("Error from the read file in getCourses function: ", err);
+				return;
+			}
+			let parsedData = JSON.parse(data);
+			console.log("parsedData from read file: ", parsedData);
+			res.json(parsedData);
+		});
+	},
+	getOneCourse: (req, res, next) => {
+		fs.readFile(DATA_DIR, "UTF-8", (err, data) => {
+			if (err) {
+				console.error("Error from the read file in getCourses function: ", err);
+				return;
+			}
+			let parsedData = JSON.parse(data);
+			console.log("parsedData from read file: ", parsedData);
+
+			let specificCourse = parsedData.find(function (c) {
+				return c.id === parseInt(req.params.id);
+			});
+
+			if (!specificCourse) {
+				res.status(404).send("The course with given ID was not found");
+			}
+			res.send(specificCourse);
+		});
+	},
 };
 
 module.exports = controllers;
--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,13 @@
-const controllers = require('./controllers.js');
-const express = require('express');
+const controllers = require("./controllers.js");
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', controllers.hello);
+router.get("/", controllers.hello);
+
+router.get("/courses", controllers.getCourses);
+router.get("/courses/:id", controllers.getOneCourse);
 
 // write your routes
-
 
 module.exports = router;
